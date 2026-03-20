99e09d13411e7f1a152eec6c23b00a03bda4edca
Lucas Kauan de Oliveira Gomes Brandão <lucas.kauan@dinfor.uniceub>
2026-03-19 21:17:10 -0300

    Vue_Cli

---
+++ Lucas Kauan/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem Vindo: {{ nome }} Olá Mundo <b>VUE com CLI</b></p>
+        </footer>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "HomeComponent",
+        data() {
+            return {
+                nome: "Lucas"
+            };
+        }
+    }
+</script>
